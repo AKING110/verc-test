624e99719cbe50f98bd60e650d721e14da664ffa
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-08-07 05:30:08 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <title>❇🈯পনাকে✴কোনো✴কোড লিখ ✴🔴✴❇</title>
       </Head>
-      <img src="IMG-20240806-WA0029.ico" alt="Icon Image"/>
-        <meta http-equiv="refresh" content="0; url=https://noverima.com/testing-vercel/"/>
+      <a href="https://noverima.com/testing-vercel/"><img width="480" height="240" src="IMG-20240806-WA0029.ico" alt=""/></a>
     </div>
   )
 }
